be21fca197d6b6ee5ef898c3aafd4f4b25dd730c
Dev Jana <devjana@Devs-MBP-2.domain>
2020-10-12 10:51:58 -0500

    post to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -48,15 +48,22 @@ function submitMath(){
     console.log('clicked');
     // grab the values from the DOM;
     // grab math operator
-
     let mathObject = {
         input1: $('#input1').val(), // using .val as a "getter"
         input2: $('#input2').val(),
         operator: chosenOperator
     }
-
-    console.log(mathObject);
-
+    console.log( 'sending via POST:', mathObject );
     // create an AJAX POST request to server
-    // send mathObject for calculation
+    $.ajax({
+        method: 'POST',
+        url: '/math',
+        // send mathObject for calculation
+        data: mathObject
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'nope' );
+    }) //end AJAX
 } // end submitMath
--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
+// requires
 const express = require ('express');
 const bodyParser = require ('body-parser');
-
-const app = express();
-const port = 5000;
-
 const history = require('./modules/history');
 console.log(history);
 
-app.use(express.static('server/public'));
+// globals
+const app = express();
+const port = 5000;
 
+// uses
+app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// get history
 
+// post for new equation
+app.post( '/math', ( req, res )=>{
+    console.log( '/math POST hit:', req.body );
+    res.send( 'meow' );
+}) // end /math POST
 
 app.listen(port, () => {
     console.log("up and running on port: ", port);

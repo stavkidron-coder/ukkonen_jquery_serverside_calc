0d50c728f309adcda60a1786f751cf706fa06abf
Dev Jana <devjana@Devs-MBP-2.domain>
2020-10-12 11:48:40 -0500

    end of lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -69,31 +69,36 @@ function getHistory(){
 } // end getHistory
 
 function submitMath(){
-    console.log('clicked');
-    // grab the values from the DOM;
-    // grab math operator
-    let mathObject = {
-        input1: $('#input1').val(), // using .val as a "getter"
-        input2: $('#input2').val(),
-        operator: chosenOperator
+    console.log('submitMath clicked');
+    if( $('#input1').val() === '' || $('#input2').val() === '' || chosenOperator === '' ){
+        alert( 'please complete all fields' );
     }
-    console.log( 'sending via POST:', mathObject );
-    // create an AJAX POST request to server
-    $.ajax({
-        method: 'POST',
-        url: '/math',
-        // send mathObject for calculation
-        data: mathObject
-    }).then( function( response ){
-        console.log( 'back from server with:', response );
-        // display answer
-        let el = $( '#answer' );
-        el.empty();
-        el.append( response.answer );
-        // get history
-        getHistory();
-    }).catch( function( err ){
-        console.log( err );
-        alert( 'nope' );
-    }) //end AJAX
+    else{
+        // grab the values from the DOM;
+        // grab math operator
+        let mathObject = {
+            input1: $('#input1').val(), // using .val as a "getter"
+            input2: $('#input2').val(),
+            operator: chosenOperator
+        }
+        console.log( 'sending via POST:', mathObject );
+        // create an AJAX POST request to server
+        $.ajax({
+            method: 'POST',
+            url: '/math',
+            // send mathObject for calculation
+            data: mathObject
+        }).then( function( response ){
+            console.log( 'back from server with:', response );
+            // display answer
+            let el = $( '#answer' );
+            el.empty();
+            el.append( response.answer );
+            // get history
+            getHistory();
+        }).catch( function( err ){
+            console.log( err );
+            alert( 'nope' );
+        }) //end AJAX
+    } // end else
 } // end submitMath
